abea1f0df7e930ffea1e56728c5ac8599d83dffa
Vaclav Haken <vaclav@vaclavkabat.net>
2018-02-20 16:24:05 +0000

    repo sync

--- src/AutoRestNuGetGenerator/AutoRestNuGetGenerator/AutoRestNuGetGenerator.ps1
+++ src/AutoRestNuGetGenerator/AutoRestNuGetGenerator/AutoRestNuGetGenerator.ps1
@@ -22,11 +22,22 @@ try {
   
 
     Write-Output "Inputs..."
+	Write-Output "Working Directory: $env:SYSTEM_DEFAULTWORKINGDIRECTORY"
+	Write-Output "PSScriptRoot: $PSScriptRoot"
     Write-Output "Swagger URL: $input_SwaggerURL"
     Write-Output "Namespace: $input_Namespace"
 	Write-Output "ErrorActionPreference : $input_errorActionPreference"
 
+
+	nuget install eShopWorld.AutoRest.CreateProject -OutputDirectory $env:SYSTEM_DEFAULTWORKINGDIRECTORY
+
+	$packageDirectory = Get-ChildItem -Directory -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -Filter "eshopworld.autorest.createproject*" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+	$packageDirName = $packageDirectory.Name
+
+	Write-Output "full path to downloaded tool $env:SYSTEM_DEFAULTWORKINGDIRECTORY\$packageDirName\tools"
+
 	$env:path+=";C:\Windows\ServiceProfiles\NetworkService\AppData\Roaming\npm"
+	$env:path+=";$env:SYSTEM_DEFAULTWORKINGDIRECTORY\$packageDirName\tools"
 
 	Write-Output "invoking 'npm install -g autorest@latest'"
 	npm install -g autorest@latest
@@ -37,7 +48,7 @@ try {
 	try
 	{
 		Write-Output "Retrieving definition json from $input_SwaggerURL"
-		Invoke-WebRequest $input_SwaggerURL -o definition.json -ErrorAction Stop
+		Invoke-WebRequest $input_SwaggerURL -o $env:SYSTEM_DEFAULTWORKINGDIRECTORY/definition.json -ErrorAction Stop
 	}
 	catch
 	{
@@ -45,13 +56,15 @@ try {
         exit
 	}
 
-	autorest --input-file=definition.json --csharp --output-folder=output --namespace=$input_Namespace
-	dotnet-autorest-createproject/dotnet autorest-createproject -s definition.json -o output
+	autorest --input-file=$env:SYSTEM_DEFAULTWORKINGDIRECTORY\definition.json --csharp --output-folder=$env:SYSTEM_DEFAULTWORKINGDIRECTORY\output --namespace=$input_Namespace
 
-	pushd output
+	
+	Write-Output "Invoking 'dotnet autorest-createproject -s $env:SYSTEM_DEFAULTWORKINGDIRECTORY\definition.json -o $env:SYSTEM_DEFAULTWORKINGDIRECTORY\output'"
+	dotnet autorest-createproject -s $env:SYSTEM_DEFAULTWORKINGDIRECTORY\definition.json -o $env:SYSTEM_DEFAULTWORKINGDIRECTORY\output
+	popd
 
+	pushd $env:SYSTEM_DEFAULTWORKINGDIRECTORY\output
 	dotnet build -c release
-
 	popd
 } finally {
 	Trace-VstsLeavingInvocation $MyInvocation
