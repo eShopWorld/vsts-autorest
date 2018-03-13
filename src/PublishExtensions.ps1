e17f6e62059d37ed79a54be13d9463c18bee2a65
Vaclav Haken <vaclav@vaclavkabat.net>
2018-03-13 13:34:58 +0000

    make public

--- src/PublishExtensions.ps1
+++ src/PublishExtensions.ps1
@@ -9,7 +9,7 @@ $tfxCli = Join-Path $path "node_modules\tfx-cli\_build\tfx-cli.js"
 $ext = Get-ChildItem -Path . -Filter *.vsix -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
 if($ext) {
-    Invoke-Expression -Command "node $tfxCli extension publish --publisher $publisher --vsix '$ext' --share-with eshopworld -t $pat"
+    Invoke-Expression -Command "node $tfxCli extension publish --publisher $publisher --vsix '$ext' -t $pat"
 }else {
     Write-Warning "No extensions found!"
 }
