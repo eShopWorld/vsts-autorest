56b6f75be235ad12717d6fe8c3dd8eee5811969c
Vaclav Haken <vaclav@vaclavkabat.net>
2019-08-07 12:07:56 +0100

    working draft

--- src/AutoRestNuGetGenerator/Tests/PetStoreTestBed.ps1
+++ src/AutoRestNuGetGenerator/Tests/PetStoreTestBed.ps1
@@ -5,4 +5,5 @@ $env:input_SwaggerURL = "https://petstore.swagger.io/v2/swagger.json"
 $env:input_Namespace = "petstore"
 $env:input_AddServiceClientCredentials = "true"
 $env:SYSTEM_DEFAULTWORKINGDIRECTORY = "."
-Invoke-VstsTaskScript -ScriptBlock { . ..\AutorestNugetGenerator\AutorestNugetGenerator.ps1 }
+Set-Location ..\AutoRestNuGetGenerator
+Invoke-VstsTaskScript -ScriptBlock { . .\AutorestNugetGenerator.ps1 }
