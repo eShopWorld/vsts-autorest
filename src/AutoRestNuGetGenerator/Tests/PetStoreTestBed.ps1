bf7c5ff1cc120950e362cd444e054a6a100b504b
Fredrik Hellman <fredrik.hellman@teliacompany.com>
2020-07-17 12:12:59 +0200

    Implemented additional options for specifying a local swagger.json file and the optional to create csproject and build arguments. Additionally we clean up old definition/output files if running from own buildserver

--- src/AutoRestNuGetGenerator/Tests/PetStoreTestBed.ps1
+++ src/AutoRestNuGetGenerator/Tests/PetStoreTestBed.ps1
@@ -2,8 +2,10 @@ Import-Module ..\ps_modules\VstsTaskSdk
 
 $env:input_errorActionPreference = "CONTINUE"
 $env:input_SwaggerURL = "https://petstore.swagger.io/v2/swagger.json"
-$env:input_Namespace = "petstore"
 $env:input_AddServiceClientCredentials = "true"
+$env:input_NameSpace = "AutorestClient"
+$env:input_ClientName = "Client"
+$env:input_BuildClient = "true"
 $env:input_OpenApiV3 = "true"
 $env:SYSTEM_DEFAULTWORKINGDIRECTORY = "."
 Set-Location ..\AutoRestNuGetGenerator
